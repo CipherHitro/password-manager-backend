3c6cda01eaff314bfce5de95b05905dd972d853e
CipherHitro <rrohit2911@gmail.com>
2025-06-03 18:22:36 +0530

    console the error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,15 @@ const { getUser } = require('../services/auth')
 async function restrictToLoggedinUser(req, res, next) {
 
     const token = req.cookies?.uid;
-
+    console.log("token: ", token)
     if(!token){
+        console.log("no token")
         return res.status(401).json({message: "not authenticated"})
     }
     const user = getUser(token)
     if(!user){
         
+        console.log("no user")
         return res.status(401).json({message: "not authenticated"})
 
     }
